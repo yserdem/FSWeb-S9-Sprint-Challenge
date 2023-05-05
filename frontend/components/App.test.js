cf41abc1d2f98b63082e299a8b12298ca59c953f
Sinan <y.sinanerdem@gmail.com>
2023-05-06 02:18:35 +0300

    App çalışıyor

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,50 @@
+import { render, fireEvent, screen } from '@testing-library/react'
+import AppFunctional from "./AppFunctional"
+
+
 // Write your tests here
 test('sanity', () => {
   expect(true).toBe(false)
 })
+
+import server from './backend/mock-server'
+import React from 'react'
+import AppFunctional from './frontend/components/AppFunctional'
+import { render, fireEvent, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+jest.setTimeout(1000) // default 5000 too long for Codegrade
+const waitForOptions = { timeout: 100 }
+const queryOptions = { exact: false }
+
+let up, down, left, right, reset, submit
+let squares, coordinates, steps, message, email
+
+const updateStatelessSelectors = document => {
+  up = document.querySelector('#up')
+  down = document.querySelector('#down')
+  left = document.querySelector('#left')
+  right = document.querySelector('#right')
+  reset = document.querySelector('#reset')
+  submit = document.querySelector('#submit')
+}
+
+const updateStatefulSelectors = document => {
+  squares = document.querySelectorAll('.square')
+  coordinates = document.querySelector('#coordinates')
+  steps = document.querySelector('#steps')
+  message = document.querySelector('#message')
+  email = document.querySelector('#email')
+}
+
+const testSquares = (squares, activeIdx) => {
+  squares.forEach((square, idx) => {
+    if (idx === activeIdx) {
+      expect(square.textContent).toBe('B')
+      expect(square.className).toMatch(/active/)
+    } else {
+      expect(square.textContent).toBeFalsy()
+      expect(square.className).not.toMatch(/active/)
+    }
+  })
+}
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,28 +1,60 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 // önerilen başlangıç stateleri
 const initialMessage = ''
 const initialEmail = ''
 const initialSteps = 0
 const initialIndex = 4 //  "B" nin bulunduğu indexi
+const theGrid = [
+  "(1,1)",
+  "(2,1)",
+  "(3,1)",
+  "(2,1)",
+  "(2,2)",
+  "(2,3)",
+  "(3,1)",
+  "(3,2)",
+  "(3,3)",
+]
 
 export default function AppFunctional(props) {
   // AŞAĞIDAKİ HELPERLAR SADECE ÖNERİDİR.
   // Bunları silip kendi mantığınızla sıfırdan geliştirebilirsiniz.
 
+  const [coordState, setCoordState] = useState(initialIndex);
+  const [steps, setSteps] = useState(initialSteps);
+  const [message, setMessage] = useState(initialMessage);
+  const [mail, setMail] = useState(initialEmail)
+
   function getXY() {
     // Koordinatları izlemek için bir state e sahip olmak gerekli değildir.
     // Bunları hesaplayabilmek için "B" nin hangi indexte olduğunu bilmek yeterlidir.
+    return theGrid[coordState]
   }
 
-  function getXYMesaj() {
+  function getXYMesaj(yon) {
     // Kullanıcı için "Koordinatlar (2, 2)" mesajını izlemek için bir state'in olması gerekli değildir.
     // Koordinatları almak için yukarıdaki "getXY" helperını ve ardından "getXYMesaj"ı kullanabilirsiniz.
     // tamamen oluşturulmuş stringi döndürür.
+
+    if (yon === "left") {
+      setMessage("Sola gidemezsiniz")
+    } else if (yon === "right") {
+      setMessage("Sağa gidemezsiniz")
+    } else if (yon === "up") {
+      setMessage("Yukarı gidemezsiniz")
+    } else if (yon === "down") {
+      setMessage("Aşağı gidemezsiniz")
+    }
   }
 
   function reset() {
     // Tüm stateleri başlangıç ​​değerlerine sıfırlamak için bu helperı kullanın.
+    setCoordState(initialIndex);
+    setSteps(initialSteps);
+    setMessage(initialMessage);
+    setMail(initialEmail);
   }
 
   function sonrakiIndex(yon) {
@@ -34,43 +66,97 @@ export default function AppFunctional(props) {
   function ilerle(evt) {
     // Bu event handler, "B" için yeni bir dizin elde etmek üzere yukarıdaki yardımcıyı kullanabilir,
     // ve buna göre state i değiştirir.
+
+    if (evt === "left" && coordState % 3 !== 0 ) {
+      setSteps(steps + 1);
+      setCoordState(coordState - 1);
+    } else if (evt === "right" && coordState % 3 !== 2 ) {
+      setSteps(steps + 1);
+      setCoordState(coordState + 1);
+    } else if (evt === "up" && coordState - 3 >= 0 ) {
+      setSteps(steps + 1);
+      setCoordState(coordState - 3);
+    } else if (evt === "down" && coordState +3 <= 8 ) {
+      setSteps(steps + 1);
+      setCoordState(coordState + 3);
+    } else {
+      getXYMesaj(evt)
+    }
   }
 
   function onChange(evt) {
     // inputun değerini güncellemek için bunu kullanabilirsiniz
+    evt.preventDefault();
+    setMail(evt.target.value)
   }
 
   function onSubmit(evt) {
     // payloadu POST etmek için bir submit handlera da ihtiyacınız var.
+    evt.preventDefault();
+
+    const theData = {
+      x: theGrid[coordState][1],
+      y: theGrid[coordState][3],
+      steps: steps,
+      email: mail,
+    };
+    console.log(theData);
+
+    setMail(initialEmail);
+
+    const config = {
+      method: "post",
+      url: "http://localhost:9000/api/result",
+      headers: {
+        "Content-Type" : "application/json",
+      },
+      data: theData
+    };
+
+    axios(config)
+      .then(function (response) {
+        setMessage(response.data.message);
+      })
+      .catch(function (error) {
+        setMessage(error.response.data.message)
+      })
   }
 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Koordinatlar (2, 2)</h3>
-        <h3 id="steps">0 kere ilerlediniz</h3>
+        <h3 id="coordinates">Koordinatlar {getXY()} </h3>
+        <h3 id="steps">{steps} kere ilerlediniz</h3>
       </div>
       <div id="grid">
         {
           [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
+            <div key={idx} className={`square${idx === coordState ? ' active' : ''}`}>
+              {idx === coordState ? 'B' : null}
             </div>
           ))
         }
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{message}</h3>
       </div>
       <div id="keypad">
-        <button id="left">SOL</button>
-        <button id="up">YUKARI</button>
-        <button id="right">SAĞ</button>
-        <button id="down">AŞAĞI</button>
-        <button id="reset">reset</button>
+        <button onClick={e => ilerle(e.target.id)} id="left">SOL</button>
+        <button onClick={e => ilerle(e.target.id)} id="up">YUKARI</button>
+        <button onClick={e => ilerle(e.target.id)} id="right">SAĞ</button>
+        <button onClick={e => ilerle(e.target.id)} id="down">AŞAĞI</button>
+        <button onClick={() => reset()} id="reset">reset</button>
       </div>
-      <form>
-        <input id="email" type="email" placeholder="email girin"></input>
+      <form onSubmit={onSubmit}>
+        <input 
+        id="email" 
+        type="email" 
+        placeholder="email girin"
+        onChange={e => onChange(e)}
+        value={mail}
+        >
+
+        </input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
